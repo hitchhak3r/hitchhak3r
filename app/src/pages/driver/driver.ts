15ed39a3300953d00f58b5de3f2607e005a3b92c
Tommy Santerre <tommy.santerre@gmail.com>
2017-03-11 11:26:22 -0500

    Sort offers by distance to drive

--- app/src/pages/driver/driver.ts
+++ app/src/pages/driver/driver.ts
@@ -3,49 +3,56 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
 import { DriverPickupPage } from '../driver-pickup/driver-pickup';
-import { AngularFire, FirebaseListObservable } from '../../../node_modules/angularfire2'
+import { AngularFire } from '../../../node_modules/angularfire2'
+import { IOffer } from "../../model/IOffer";
+import 'rxjs/Rx';
 
-/*
- Generated class for the Driver page.
-
- See http://ionicframework.com/docs/v2/components/#navigation for more info on
- Ionic pages and navigation.
- */
 declare var google;
 @Component({
   selector: 'page-driver',
   templateUrl: 'driver.html'
 })
 export class DriverPage {
+  availableOffers: any;
+  position: any;
 
-  availableOffers : FirebaseListObservable<any[]>;
-  position : any;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private af:AngularFire) {
-    var self = this;
-    try{
-      Geolocation.getCurrentPosition().then(val => self.position = val.coords);
+  constructor(public navCtrl: NavController, public navParams: NavParams, private af: AngularFire) {
+    try {
+      Geolocation.getCurrentPosition()
+        .then(val => this.position = val.coords)
+        .then(() => this.updateOffers());
     }
-    catch(ex){
+    catch (ex) {
     }
 
-    this.availableOffers = af.database.list("/AvailableOffers");
+    this.updateOffers();
+  }
+
+  updateOffers() {
+    this.availableOffers = this.af.database.list("/AvailableOffers").map(offers => (offers.map(offer => {
+      let distance = this.getHitchHikerDistance(offer);
+      return {
+        offer,
+        distance,
+        distanceDescription: distance === NaN ? "n'est pas disponible" : `${distance} km`
+      };
+    }).sort((a, b) => a.distance > b.distance)));
   }
 
   chooseItem(item: any) {
-    this.navCtrl.push(DriverPickupPage,{destination: item});
+    this.navCtrl.push(DriverPickupPage, { destination: item });
   }
 
-  getHitchHikerDistance(item){
-    if(this.position){
-      var dest = item.Hitchhacker.GeoPosition
-      var dist = google.maps.geometry.spherical.computeDistanceBetween (
+  getHitchHikerDistance(item: IOffer): number {
+    if (this.position && item.Hitchhacker) {
+      const dest = item.Hitchhacker.GeoPosition;
+      let dist = google.maps.geometry.spherical.computeDistanceBetween(
         new google.maps.LatLng(this.position.latitude, this.position.longitude),
         new google.maps.LatLng(dest.lat, dest.lng));
 
-      return dist.toFixed(2) + " km";
+      return dist.toFixed(2);
     }
-    return "non disponible";
+    return NaN;
   }
 
   ionViewDidLoad() {
