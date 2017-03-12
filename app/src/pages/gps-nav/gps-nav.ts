685f35d372e3e5ce83d94c197bcb203119999a43
Tommy Santerre <tommy.santerre@gmail.com>
2017-03-12 08:03:39 -0400

    Remove useless import

--- app/src/pages/gps-nav/gps-nav.ts
+++ app/src/pages/gps-nav/gps-nav.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { DriverPage } from "../driver/driver";
 
 /*
   Generated class for the GpsNav page.
